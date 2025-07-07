35ae896c0ec854b44b2677d0b821be9e7e556a63
LovelyDeemo <nanibanks06@gmail.com>
2025-07-07 12:25:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const config = {
   clientId: "dfb00f68051240de86117f05e407ea21",
-  redirectUri: "https://LovelyDeemo.github.io/nzxt-kraken-display/",
+  redirectUri: "https://lovelydeemo.github.io/nzxt-kraken-display/",
   theme: "default",
 };
-
